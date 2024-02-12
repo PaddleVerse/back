da139a92d98867f2701f40540366e860bc31ee92
Mohamed Jarboua <mjarboua@e1r6p5.1337.ma>
2024-02-12 13:51:20 +0100

    standard push

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -10,27 +10,52 @@ import { Appearance, Prisma, user } from "@prisma/client";
 import { Socket, Server } from "socket.io";
 import { ChatService } from "./chat.service";
 import { UserService } from "src/user/user.service";
+import { SocketAddress } from "net";
 
 @WebSocketGateway({ namespace: "channel-convo" })
 export class ChatGateway {
   constructor(
     private readonly chatService: ChatService,
     private readonly userService: UserService
-  ) {}
+  ) {
+    this.connectedClients = new Map<number, string>();
+  }
   @WebSocketServer()
   server: Server;
   connectedClients: Map<number, string>;
 
-  @SubscribeMessage('new-socket-connection')
-  handleConnection(@ConnectedSocket() socket: Socket, @Body('user-info') user: user) {
-    this.connectedClients[user.id] = socket.id;
-    console.log(this.connectedClients.size);
+  /**
+   *
+   * @param socket the socket that initiated the handshake
+   * @description this function handles the logic of the connection of the socket to the server
+   */
+  async handleConnection(@ConnectedSocket() socket: Socket) {
+    try {
+      // get the user by id, if the user doesnt exist the socket assignment line will raise an error to be thrown later
+      const user = await this.userService.getUserById(
+        Number(socket.handshake.query.userId)
+      );
+      try {
+        this.connectedClients.set(user.id, socket.id);
+      } catch (error) {
+        throw new Error(`error connecting socket to unregistered user`);
+      }
+    } catch (error: any) {
+      socket.emit("connection-error", { error: error.toString() });
+    }
   }
 
+  /**
+   *
+   * @param channelInfo
+   * @param user
+   * @param socket
+   * @returns an acknoledgement to the client that the channel has been created
+   */
   @SubscribeMessage("join-channel")
   async joinChannel(
     @Body("channel-info") channelInfo: any,
-    @Body("user-info") user: user,
+    @Body("user-info") user: user, // this one can be fixed by using query containing user id
     @ConnectedSocket() socket: Socket
   ) {
     if (!channelInfo || !user) {
@@ -73,5 +98,10 @@ export class ChatGateway {
     }
     return "reached end of structutre";
     // the channel can have three states, private (hidden from the groupds interface), protected (not hidden but has a password), or public check those and check if the user has all the things needed to join
+  }
+
+  @SubscribeMessage('kick-from-channel')
+  async kickFromChannel(@ConnectedSocket() socket: Socket) {
+
   }
 }
