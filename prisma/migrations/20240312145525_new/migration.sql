9f138b5a1fa08749634103f48f4cb6f1a0cc17fd
Mohamed Jarboua <mjarboua@e1r7p4.1337.ma>
2024-03-12 16:40:00 +0000

    fixed something

---
+++ prisma/migrations/20240312145525_new/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "channel" ADD COLUMN     "picture" TEXT DEFAULT 'https://www.freepik.com/free-vector/flat-illustration-people-looking-up_14602291.htm#query=group%20cartoon&position=7&from_view=keyword&track=ais&uuid=5cdd0157-2810-4e64-b8ab-6d030cddd63e';
