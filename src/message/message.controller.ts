fe9834034836e97fb5130072e9485a2f3d61e6a7
Mohamed Jarboua <mjarboua@e1r6p4.1337.ma>
2024-04-25 20:46:49 +0100

    now sending picture in messages and stuff

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,4 +1,14 @@
-import { Body, ConsoleLogger, Controller, Get, HttpException, Post, Query } from "@nestjs/common";
+import {
+  Body,
+  ConsoleLogger,
+  Controller,
+  Get,
+  HttpException,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
 import { MessageService } from "./message.service";
 import { Prisma, channel, message, user } from "@prisma/client";
 import { privateDecrypt } from "crypto";
@@ -6,6 +16,8 @@ import { ChannelsService } from "src/channels/channels.service";
 import { ConversationsService } from "src/conversations/conversations.service";
 import { UserService } from "src/user/user.service";
 import { ParticipantsService } from "src/participants/participants.service";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { MulterFile } from "multer";
 
 @Controller("message")
 export class MessageController {
@@ -36,7 +48,10 @@ export class MessageController {
         if (!u) {
           throw new HttpException("User does not exist", 404);
         }
-        const participants = await this.participantsService.getParticipantByIds(ch.id, Number(users1));
+        const participants = await this.participantsService.getParticipantByIds(
+          ch.id,
+          Number(users1)
+        );
         if (!participants) {
           throw new HttpException("User is not a participant", 404);
         }
@@ -46,17 +61,94 @@ export class MessageController {
         });
         return message;
       } else {
-        const u1 = await this.conversationService.userService.getUserById(Number(users1));
-        const u2 = await this.conversationService.userService.getUserById(Number(users2));
+        const u1 = await this.conversationService.userService.getUserById(
+          Number(users1)
+        );
+        const u2 = await this.conversationService.userService.getUserById(
+          Number(users2)
+        );
         if (!u1 || !u2) {
           throw new HttpException("User does not exist", 404);
         }
-        const co = await this.conversationService.getConversation(Number(users1), Number(users2));
+        const co = await this.conversationService.getConversation(
+          Number(users1),
+          Number(users2)
+        );
         if (!co) {
           throw new HttpException("Conversation does not exist", 404);
         }
         const message = await this.messageService.createMessage({
           ...m,
+          conv: { connect: { id: co.id } },
+        });
+        return message;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Post("/image")
+  @UseInterceptors(FileInterceptor("image"))
+  async createImageMessage(
+    @UploadedFile() file: MulterFile,
+    @Query("channel") channel: string,
+    @Query("sender") users1: string,
+    @Query("reciever") users2: string
+  ) {
+    try {
+      // console.log(file);
+      console.log(users1, users2);
+      if (channel) {
+        const ch = !isNaN(Number(channel))
+          ? await this.channelService.getChannelById(Number(channel))
+          : await this.channelService.getChannelByName(channel);
+        if (!ch) {
+          throw new HttpException("Channel does not exist", 404);
+        }
+        const u = await this.userService.getUser(Number(users1));
+        if (!u) {
+          throw new HttpException("User does not exist", 404);
+        }
+        const participants = await this.participantsService.getParticipantByIds(
+          ch.id,
+          Number(users1)
+        );
+        if (!participants) {
+          throw new HttpException("User is not a participant", 404);
+        }
+        const url = await this.userService.uploadImage(file);
+        const message = await this.messageService.createMessage({
+          content: url,
+          content_type: "image",
+          sender_picture: u.picture,
+          sender_id: u.id,
+          channel: { connect: { id: ch.id } },
+        });
+        return message;
+      } else {
+        const u1 = await this.conversationService.userService.getUserById(
+          Number(users1)
+        );
+        const u2 = await this.conversationService.userService.getUserById(
+          Number(users2)
+        );
+        if (!u1 || !u2) {
+          throw new HttpException("User does not exist", 404);
+        }
+        const co = await this.conversationService.getConversation(
+          Number(users1),
+          Number(users2)
+        );
+        if (!co) {
+          throw new HttpException("Conversation does not exist", 404);
+        }
+        const url = await this.userService.uploadImage(file);
+        const message = await this.messageService.createMessage({
+          content: url,
+          content_type: "image",
+          sender_picture: u1.picture,
+          sender_id: u1.id,
           conv: { connect: { id: co.id } },
         });
         return message;
