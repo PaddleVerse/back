7299d2b987313187248acfa0fb88f92625cb3236
Mohamed Jarboua <mjarboua@e1r6p4.1337.ma>
2024-04-29 19:04:56 +0100

    fixeed somethings

---
+++ prisma/migrations/20240429172926_test/migration.sql
@@ -0,0 +1,227 @@
+-- CreateEnum
+CREATE TYPE "Appearance" AS ENUM ('protected', 'private', 'public');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'MOD', 'MEMBER');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ONLINE', 'OFFLINE', 'ON_GAME');
+
+-- CreateEnum
+CREATE TYPE "Req" AS ENUM ('RECIVED', 'SEND');
+
+-- CreateEnum
+CREATE TYPE "FriendshipStatus" AS ENUM ('PENDING', 'ACCEPTED', 'BLOCKED');
+
+-- CreateEnum
+CREATE TYPE "N_Type" AS ENUM ('MESSAGE', 'REQUEST');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "googleId" TEXT,
+    "fortytwoId" INTEGER,
+    "username" TEXT NOT NULL,
+    "name" TEXT,
+    "nickname" TEXT DEFAULT 'zuse',
+    "password" TEXT NOT NULL,
+    "picture" TEXT DEFAULT 'https://res.cloudinary.com/dxxlqdwxb/image/upload/v1713806275/kx6iknqyvu0uyqhhpfro.jpg',
+    "banner_picture" TEXT DEFAULT 'https://res.cloudinary.com/dxxlqdwxb/image/upload/v1713526102/zxwritc0rqvtjvcwbqiv.jpg',
+    "status" "Status" NOT NULL DEFAULT 'OFFLINE',
+    "level" INTEGER DEFAULT 600,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "twoFa" BOOLEAN DEFAULT false,
+    "twoFaSecret" TEXT,
+    "first_time" BOOLEAN DEFAULT true,
+    "notified" BOOLEAN DEFAULT false,
+    "coins" INTEGER DEFAULT 1000,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SearchHistory" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "SearchHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Friendship" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER DEFAULT 0,
+    "friendId" INTEGER DEFAULT 0,
+    "status" "FriendshipStatus" NOT NULL,
+    "request" "Req" DEFAULT 'SEND',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "achievement" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "description" TEXT,
+    "picture" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" INTEGER DEFAULT 0,
+
+    CONSTRAINT "achievement_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "game_history" (
+    "id" SERIAL NOT NULL,
+    "user_a_id" INTEGER DEFAULT 0,
+    "user_b_id" INTEGER DEFAULT 0,
+    "user_a_score" INTEGER DEFAULT 0,
+    "user_b_score" INTEGER DEFAULT 0,
+    "start_time" TIMESTAMP(3),
+    "end_time" TIMESTAMP(3),
+
+    CONSTRAINT "game_history_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "channel" (
+    "id" SERIAL NOT NULL,
+    "picture" TEXT DEFAULT 'http://localhost:8080/1709546788254-test7.png',
+    "topic" TEXT DEFAULT 'here to have fun and make friends, no toxicity allowed',
+    "key" TEXT,
+    "state" "Appearance" DEFAULT 'public',
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "channel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "message" (
+    "id" SERIAL NOT NULL,
+    "channel_id" INTEGER,
+    "sender_id" INTEGER DEFAULT 0,
+    "sender_picture" TEXT,
+    "conversation_id" INTEGER,
+    "content" TEXT,
+    "content_type" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ban_list" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER DEFAULT 0,
+    "channel_id" INTEGER DEFAULT 0,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ban_list_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "channel_participant" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER DEFAULT 0,
+    "channel_id" INTEGER DEFAULT 0,
+    "role" "Role" DEFAULT 'MEMBER',
+    "mute" BOOLEAN DEFAULT false,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "channel_participant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "conversation" (
+    "id" SERIAL NOT NULL,
+    "user_a_id" INTEGER DEFAULT 0,
+    "user_b_id" INTEGER DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notification" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER DEFAULT 0,
+    "sender_name" TEXT,
+    "sender_picture" TEXT,
+    "sender_id" INTEGER DEFAULT 0,
+    "type" "N_Type",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "paddle" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER DEFAULT 0,
+    "image" TEXT,
+    "color" TEXT,
+    "enabled" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "paddle_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ball" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER DEFAULT 0,
+    "image" TEXT,
+    "color" TEXT,
+    "enabled" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ball_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_googleId_key" ON "user"("googleId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_fortytwoId_key" ON "user"("fortytwoId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "channel_name_key" ON "channel"("name");
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "achievement" ADD CONSTRAINT "achievement_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "message" ADD CONSTRAINT "message_conversation_id_fkey" FOREIGN KEY ("conversation_id") REFERENCES "conversation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "message" ADD CONSTRAINT "message_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "channel"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ban_list" ADD CONSTRAINT "ban_list_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ban_list" ADD CONSTRAINT "ban_list_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "channel"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "channel_participant" ADD CONSTRAINT "channel_participant_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "channel_participant" ADD CONSTRAINT "channel_participant_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "channel"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notification" ADD CONSTRAINT "notification_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "paddle" ADD CONSTRAINT "paddle_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ball" ADD CONSTRAINT "ball_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
