9edb52a7b557ff082f2ce8285206ecbc07ff78c7
Mohamed Jarboua <mjarboua@e3r1p6.1337.ma>
2024-05-19 12:57:49 +0100

    adding dto to channel post and put methods

---
+++ src/channels/dto/createChannelDto.ts
@@ -0,0 +1,36 @@
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  IsAlphanumeric,
+  Matches,
+} from "class-validator";
+
+export class CreateChannelDto {
+
+  @IsString({ message: "Password must be a string" })
+  @IsNotEmpty({ message: "Password cannot be empty" })
+  @MinLength(6, { message: "Password must be at least 6 characters long" })
+  @MaxLength(20, { message: "Password cannot be longer than 20 characters" })
+  @IsAlphanumeric()
+  password: string;
+
+  @IsString({ message: "Name must be a string" })
+  @IsNotEmpty({ message: "Name cannot be empty" })
+  @MinLength(3, { message: "Name must be at least 3 characters long" })
+  @MaxLength(20, { message: "Name cannot be longer than 20 characters" })
+  @Matches(/^[a-zA-Z_]+$/, {
+    message: "Name can only contain alphabet characters and underscores",
+  })
+  name: string;
+
+  @IsString({ message: "Middle name must be a string" })
+  @IsNotEmpty({ message: "Middle name cannot be empty" })
+  @MinLength(3, { message: "Middle name must be at least 3 characters long" })
+  @MaxLength(20, { message: "Middle name cannot be longer than 20 characters" })
+  @Matches(/^[a-zA-Z_]+$/, {
+    message: "Middlename can only contain alphabet characters and underscores",
+  })
+  topic: string;
+}
